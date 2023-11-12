811961dcc5b109329073cec8392d50d56d9fe8cd
JorgeGarcia855 <jlgarciap@unbosque.edu.co>
2023-11-11 20:24:58 -0500

    fullstack prep

--- src/components/organisms/header.rs
+++ src/components/organisms/header.rs
@@ -7,7 +7,7 @@ pub fn Header() -> impl IntoView {
 		<header>
 			<nav class="bg-white border-gray-200 dark:bg-gray-900">
 			<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-				<A href="" class="flex items-center"> <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"Tienda Generica"}</span></A>
+				<A href="/home" class="flex items-center"> <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">"Tienda Generica"</span></A>
 				<div class="hidden w-full md:block md:w-auto" id="navbar-default">
 				<ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 					<li>
